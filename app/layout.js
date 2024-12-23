83d7eadc7c9d3a80ead50aacd27ecca14b1b9441
Aman <singh2004aman@gmail.com>
2024-12-24 03:33:03 +0530

    v0.1

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,17 @@
 import "./globals.css";
+import { Poppins } from 'next/font/google';
+
+
+const poppins = Poppins({
+  weight: ['400', '600'],  // Include the font weights you need
+  subsets: ['latin'],      // You can add more subsets if necessary
+  display: 'swap',         // Improves font loading performance
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         {children}
       </body>
     </html>
